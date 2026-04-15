44873448fd4fceebf39177dd296219c429b4a113
nabi-yue <nabiyuu.yue@gmail.com>
2026-04-15 12:32:09 +0800

    Added basic files, calculator logic and visuals

---
+++ script.js
@@ -0,0 +1,32 @@
+let numOne
+let numTwo
+let operationSymbol
+
+function addition(numOne, numTwo){
+    return numOne + numTwo;
+}
+
+function subtraction(numOne, numTwo){
+    return numOne - numTwo;
+}
+
+function multiplication(numOne, numTwo){
+    return numOne * numTwo;
+}
+
+function division(numOne, numTwo){
+    return numOne/numTwo;
+}
+
+function operate(numOne, numTwo, operationSymbol){
+    switch(operationSymbol){
+        case '+':
+            return addition(numOne, numTwo);
+        case '-':
+            return subtraction(numOne, numTwo);
+        case '*':
+            return multiplication(numOne, numTwo);
+        case '/':
+            return division(numOne, numTwo);
+    }
+}
