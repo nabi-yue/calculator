d2c649cbf09c8e7b50ab9a5de59e503d918c2e49
nabi-yue <nabiyuu.yue@gmail.com>
2026-04-26 17:04:56 +0800

    finished the functionality of the calculator

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ let firstToDisplay = document.createElement("p");
 let secondToDisplay = document.createElement("p");
 let operationToDisplay = document.createElement("p");
 const numberPad = document.querySelectorAll(".number-buttons");
+let firstSelection = [];
+let secondSelection = [];
 let numClicked;
 let chosenOperator;
-let numberOfClicks = 0;
+let numberOfOperations = 0;
 let numOne;
 let numTwo;
 let operationSymbol;
+let typeOfOperation;
+let result;
 
 function addition(numOne, numTwo){
     return numOne + numTwo;
@@ -31,49 +35,157 @@ function division(numOne, numTwo){
 function operate(numOne, numTwo, operationSymbol){
     switch(operationSymbol){
         case '+':
-            return addition(numOne, numTwo);
+            return result = Math.round((addition(numOne, numTwo))*100)/100;
         case '-':
-            return subtraction(numOne, numTwo);
-        case '*':
-            return multiplication(numOne, numTwo);
-        case '/':
-            return division(numOne, numTwo);
+            return result = Math.round((subtraction(numOne, numTwo))*100)/100;
+        case 'x':
+            return result = Math.round((multiplication(numOne, numTwo))*100)/100;
+        case '÷':
+            if(numOne == 0 || numTwo == 0){
+                return result = "NOPE"
+            } else {
+                return result = Math.round((division(numOne, numTwo))*100)/100;
+            }
+            
     }
 }
 
+function getNumber(numArray){
+    if(numberOfOperations==0){
+        return numOne = Number(numArray.join(''))
+    } else if(numberOfOperations==1){
+        return numTwo = Number(secondSelection.join(''))
+    }
+}
+
+function clearAll(){
+    firstSelection = []
+                numOne = 0
+                secondSelection = []
+                numTwo = 0
+                numberOfOperations = 0
+                typeOfOperation = '';
+                result = 0
+}
 
 numberPad.forEach((number)=>{
     number.addEventListener("click", ()=> {
         numClicked = parseInt(number.textContent, 10);
-        
-        if(numberOfClicks==0){
-            numOne = numClicked
 
-            
-            firstToDisplay.textContent = numOne
-            screen.appendChild(firstToDisplay);
+        if(numberOfOperations==0){
+            firstSelection.push(numClicked);
+            displayedNumber.textContent = firstSelection.join('')
+            console.log(firstSelection)
+        } if(numberOfOperations==1){
+            secondSelection.push(numClicked);
+            displayedNumber.textContent = numOne+typeOfOperation+secondSelection.join('')
+            console.log(secondSelection)
+        }
 
-            numberOfClicks++
-        } else if(numberOfClicks==1){
-            numTwo = numClicked
 
-            secondToDisplay.textContent = numTwo
-            screen.appendChild(secondToDisplay);
 
-            numberOfClicks = 0;
-        } 
-
-        console.log(numOne);
-        console.log(numTwo);
     })
 })
 
 signPad.forEach((sign)=>{
     sign.addEventListener("click", () => {
-        chosenOperator = sign.textContent.trim();
-        if(numberOfClicks==1){
-            operationToDisplay.textContent = chosenOperator;
-            screen.appendChild(operationToDisplay);
+        chosenOperator = sign.textContent.trim();        
+        console.log(chosenOperator)
+        switch(chosenOperator){
+            case '+':
+                if(numberOfOperations==0){
+                    typeOfOperation = '+'
+                    getNumber(firstSelection)
+                    displayedNumber.textContent = numOne+typeOfOperation
+                    numberOfOperations++
+                } else if (numberOfOperations==1){
+                    getNumber(secondSelection)
+                    operate(numOne, numTwo, typeOfOperation)
+                    displayedNumber.textContent = result
+                    typeOfOperation = '+'
+                    console.log(result)
+                    numOne = result
+                    secondSelection = []
+                    numTwo = 0
+                    numberOfOperations = 1
+                }
+                break;
+            case '-':
+                
+                if(numberOfOperations==0){
+                    typeOfOperation = '+'
+                    getNumber(firstSelection)
+                    displayedNumber.textContent = numOne+typeOfOperation
+                    numberOfOperations++
+                } else if (numberOfOperations==1){
+                    getNumber(secondSelection)
+                    operate(numOne, numTwo, typeOfOperation)
+                    displayedNumber.textContent = result
+                    typeOfOperation = '-'
+                    console.log(result)
+                    numOne = result
+                    secondSelection = []
+                    numTwo = 0
+                    numberOfOperations = 1
+                }
+                break;
+            case 'x':
+                if(numberOfOperations==0){
+                    typeOfOperation = 'x'
+                    getNumber(firstSelection)
+                    displayedNumber.textContent = numOne+typeOfOperation
+                    numberOfOperations++
+                } else if (numberOfOperations==1){
+                    getNumber(secondSelection)
+                    operate(numOne, numTwo, typeOfOperation)
+                    displayedNumber.textContent = result
+                    typeOfOperation = 'x'
+                    console.log(result)
+                    numOne = result
+                    secondSelection = []
+                    numTwo = 0
+                    numberOfOperations = 1
+                }
+                break;
+            case '÷':
+                if(numberOfOperations==0){
+                    typeOfOperation = '÷'
+                    getNumber(firstSelection)
+                    displayedNumber.textContent = numOne+typeOfOperation
+                    numberOfOperations++
+                } else if (numberOfOperations==1){
+                    getNumber(secondSelection)
+                    operate(numOne, numTwo, typeOfOperation)
+                    displayedNumber.textContent = result
+                    typeOfOperation = '÷'
+                    console.log(result)
+                    numOne = result
+                    secondSelection = []
+                    numTwo = 0
+                    numberOfOperations = 1
+                }
+                break;
+            case '=':
+                getNumber(secondSelection)
+                operate(numOne, numTwo, typeOfOperation)
+                displayedNumber.textContent = result
+                console.log(result)
+                clearAll()
+                break;
+            case '<-':
+                if(numberOfOperations==0){
+                    firstSelection.pop();
+                    displayedNumber.textContent = firstSelection.join('')
+                    
+                } else if (numberOfOperations==1){
+                    secondSelection.pop();
+                    displayedNumber.textContent = numOne+typeOfOperation+secondSelection.join('')
+                }
+                break;
+            case 'clear':
+                clearAll()
+                console.log(firstSelection,numOne, numTwo,result)
+                break;
         }
     })
 })
